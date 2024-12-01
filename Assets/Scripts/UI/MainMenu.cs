abdf6c034d0721501a73078f54b242687db82d91
unknown <s26957@pjwstk.edu.pl>
2024-12-01 08:02:33 +0100

    how to play

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void NewGame(){
+        Debug.Log("mainmenuklikam");
         SceneManager.LoadScene("Scenes/Intro");
     }
 
